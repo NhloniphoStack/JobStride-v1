39ca69d8b71fc6985cc3e5546607bd8b1e9d6bb7
NhloniphoStack <devstack02@icloud.com>
2026-09-11 00:50:04 +0200

    Another deployment issue fix

--- routes/api.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { getUser } from '../controllers/apiController.js'
-import { requireAuth } from '../middleware/requireAuth.js'
-
-export const apiRouter = express.Router()
-
-apiRouter.get('/me', requireAuth, getUser)
